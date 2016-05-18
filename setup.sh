91bf5e17f82863bf3442e6d49c63c5fc8de9f1a6
kkb <kkbyu0@gmail.com>
2016-05-18 20:58:56 +0900

    add .ideavimrc for mac

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@
 ln -s $HOME/dotfiles/.gitignore $HOME/.gitignore
 ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/idea/.ideavimrc $HOME/.ideavimrc
