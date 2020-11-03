338ad7c4205b442ea9ce28e481c560c5c84d1714
kkb <kkbyu0@gmail.com>
2020-11-03 23:14:11 +0900

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/zsh/.zprofile $HOME/.zprofile
 ln -s $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/idea/.ideavimrc $HOME/.ideavimrc
+ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 
 mkdir -p $HOME/.config/git/ 2>/dev/null
 ln -s $HOME/dotfiles/.gitignore $HOME/.config/git/ignore
