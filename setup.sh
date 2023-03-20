03736f1f14dbe5e23d359f61e841319385b2c2a6
kkb <kkbyu0@gmail.com>
2023-03-20 21:45:24 +0900

    fix typo

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ ln -s $HOME/dotfiles/zsh/.zprofile $HOME/.zprofile
 ln -s $HOME/dotfiles/vim/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/idea/.ideavimrc $HOME/.ideavimrc
 ln -s $HOME/dotfiles/brew/Brewfile $HOME/Brewfile
-ln -s $HOME/dotfiles/item2/com.googlecode.item2.plist $HOME/com.googlecode.item2.plist
+ln -s $HOME/dotfiles/iterm2/com.googlecode.iterm2.plist $HOME/com.googlecode.iterm2.plist
 
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 mkdir -p $HOME/.config/git/ 2>/dev/null
